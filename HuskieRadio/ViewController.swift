a95a5d88edf4f078a8fa01ac5ce14f851cf3f2ea
Sava Danchev <sdanchev4090@stu.d214.org>
2022-10-28 15:40:13 -0500

    Initial Commit

---
+++ HuskieRadio/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HuskieRadio
+//
+//  Created by Sava Danchev on 10/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
