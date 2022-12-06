5c898982beeccf9499afc923e19358c07d5fa834
Sava Danchev <sdanchev4090@stu.d214.org>
2022-12-06 11:24:07 -0600

    Volume and Play/Pause Animations Prep

--- HuskieRadio/ViewController.swift
+++ HuskieRadio/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var playPauseButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
 
-
+    @IBAction func playPausePressed(_ sender: Any) {
+    }
+    
+    @IBAction func volumePressed(_ sender: Any) {
+    }
 }
 
