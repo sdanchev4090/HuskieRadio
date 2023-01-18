22c3748c329b9b7727455e8fce04203fc636b42c
Sava Danchev <sdanchev4090@stu.d214.org>
2023-01-18 11:20:14 -0600

    Audio Streaming Test Successful

--- HuskieRadio/ViewController.swift
+++ HuskieRadio/ViewController.swift
@@ -6,20 +6,46 @@
 //
 
 import UIKit
+import AVKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var playPauseButton: UIButton!
     let urlHQ = "https://cast3.asurahosting.com/proxy/johnhers/stream"
     let urlLQ = "https://cast3.asurahosting.com/proxy/johnhers/stream2"
+    
+    @IBOutlet weak var playPauseButton: UIButton!
+    var player: AVPlayer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        audioPlayer()
     }
 
+    func audioPlayer() {
+        do {
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback)
+            player = AVPlayer(url: URL.init(string: urlHQ)!)
+            
+            let controller = AVPlayerViewController()
+            controller.player = player
+            controller.showsPlaybackControls = false
+            self.addChild(controller)
+            let screenSize = UIScreen.main.bounds.size
+            let videoFrame = CGRect(x: 0, y: 130, width: screenSize.width, height: (screenSize.height - 130) / 2)
+            controller.view.frame = videoFrame
+            self.view.addSubview(controller.view)
+            
+            player?.play()
+        } catch {
+        }
+    }
+    
+    
+    
     @IBAction func historyPressed(_ sender: Any) {
-        let  nvc  = SongHistoryViewController(nibName: "SongHistoryViewController", bundle: nil)
+        let nvc = SongHistoryViewController(nibName: "SongHistoryViewController", bundle: nil)
     }
     @IBAction func playPausePressed(_ sender: Any) {
     }
