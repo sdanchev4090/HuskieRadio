42bd23bd2a55aa8c2dcae8fcba52c06d72d6b847
Sava Danchev <sdanchev4090@stu.d214.org>
2024-04-19 13:37:40 -0500

    Started DeviceChecker Setup

--- HuskieRadio/ViewController.swift
+++ HuskieRadio/ViewController.swift
@@ -70,6 +70,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 // MARK: - UI Setup
     
     func setup() {
+        
         // TabBar
         tabBarBG.layer.cornerRadius = 30
         tabBarBG.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
@@ -77,6 +78,10 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         // Recently Played
         recentsBG.layer.cornerRadius = 30
         recentsBG.layer.maskedCorners = [.layerMinXMinYCorner, .layerMinXMaxYCorner]
+        
+        if UIDevice.modelName == "iPad (10th generation)" {
+            tabBarBG.layer.maskedCorners = [.layerMinXMinYCorner, .layerMinXMaxYCorner, .layerMaxXMinYCorner, .layerMaxXMaxYCorner]
+        }
         
         // Song Name
 //        songTitle.frame = CGRect(x: 92, y: 636, width: 410, height: 29)
@@ -341,3 +346,64 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 //
 //
 //}
+
+
+//---------------------------------------------//
+// MARK: - Device Checker
+
+public extension UIDevice {
+
+    static let modelName: String = {
+        var systemInfo = utsname()
+        uname(&systemInfo)
+        let machineMirror = Mirror(reflecting: systemInfo.machine)
+        let identifier = machineMirror.children.reduce("") { identifier, element in
+            guard let value = element.value as? Int8, value != 0 else { return identifier }
+            return identifier + String(UnicodeScalar(UInt8(value)))
+        }
+
+        func mapToDevice(identifier: String) -> String { // swiftlint:disable:this cyclomatic_complexity
+            #if os(iOS)
+            switch identifier {
+            case "iPad2,1", "iPad2,2", "iPad2,3", "iPad2,4":      return "iPad 2"
+            case "iPad3,1", "iPad3,2", "iPad3,3":                 return "iPad (3rd generation)"
+            case "iPad3,4", "iPad3,5", "iPad3,6":                 return "iPad (4th generation)"
+            case "iPad6,11", "iPad6,12":                          return "iPad (5th generation)"
+            case "iPad7,5", "iPad7,6":                            return "iPad (6th generation)"
+            case "iPad7,11", "iPad7,12":                          return "iPad (7th generation)"
+            case "iPad11,6", "iPad11,7":                          return "iPad (8th generation)"
+            case "iPad12,1", "iPad12,2":                          return "iPad (9th generation)"
+            case "iPad13,18", "iPad13,19":                        return "iPad (10th generation)"
+            case "iPad4,1", "iPad4,2", "iPad4,3":                 return "iPad Air"
+            case "iPad5,3", "iPad5,4":                            return "iPad Air 2"
+            case "iPad11,3", "iPad11,4":                          return "iPad Air (3rd generation)"
+            case "iPad13,1", "iPad13,2":                          return "iPad Air (4th generation)"
+            case "iPad13,16", "iPad13,17":                        return "iPad Air (5th generation)"
+            case "iPad2,5", "iPad2,6", "iPad2,7":                 return "iPad mini"
+            case "iPad4,4", "iPad4,5", "iPad4,6":                 return "iPad mini 2"
+            case "iPad4,7", "iPad4,8", "iPad4,9":                 return "iPad mini 3"
+            case "iPad5,1", "iPad5,2":                            return "iPad mini 4"
+            case "iPad11,1", "iPad11,2":                          return "iPad mini (5th generation)"
+            case "iPad14,1", "iPad14,2":                          return "iPad mini (6th generation)"
+            case "iPad6,3", "iPad6,4":                            return "iPad Pro (9.7-inch)"
+            case "iPad7,3", "iPad7,4":                            return "iPad Pro (10.5-inch)"
+            case "iPad8,1", "iPad8,2", "iPad8,3", "iPad8,4":      return "iPad Pro (11-inch) (1st generation)"
+            case "iPad8,9", "iPad8,10":                           return "iPad Pro (11-inch) (2nd generation)"
+            case "iPad13,4", "iPad13,5", "iPad13,6", "iPad13,7":  return "iPad Pro (11-inch) (3rd generation)"
+            case "iPad14,3", "iPad14,4":                          return "iPad Pro (11-inch) (4th generation)"
+            case "iPad6,7", "iPad6,8":                            return "iPad Pro (12.9-inch) (1st generation)"
+            case "iPad7,1", "iPad7,2":                            return "iPad Pro (12.9-inch) (2nd generation)"
+            case "iPad8,5", "iPad8,6", "iPad8,7", "iPad8,8":      return "iPad Pro (12.9-inch) (3rd generation)"
+            case "iPad8,11", "iPad8,12":                          return "iPad Pro (12.9-inch) (4th generation)"
+            case "iPad13,8", "iPad13,9", "iPad13,10", "iPad13,11":return "iPad Pro (12.9-inch) (5th generation)"
+            case "iPad14,5", "iPad14,6":                          return "iPad Pro (12.9-inch) (6th generation)"
+            case "i386", "x86_64", "arm64":                       return "Simulator \(mapToDevice(identifier: ProcessInfo().environment["SIMULATOR_MODEL_IDENTIFIER"] ?? "iOS"))"
+            default:                                              return identifier
+            }
+            #endif
+        }
+
+        return mapToDevice(identifier: identifier)
+    }()
+
+}
